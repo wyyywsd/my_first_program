809a8eddec091c851f3e1daa0ab8c5806ca5add2
wangzq <wangzq@tajansoft.com>
2019-08-16 18:44:00 +0800

    add beego in my program

--- src/main.go
+++ src/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
+import "github.com/astaxie/beego"
 
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get(){
+	this.Ctx.WriteString("hello world")
+}
+func main(){
+	beego.Router("/", &MainController{})
+	beego.Run()
 }
